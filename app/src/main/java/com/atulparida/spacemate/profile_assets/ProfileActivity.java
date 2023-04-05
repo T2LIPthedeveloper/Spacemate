f5471316462b93b264c062fd6e3b99e8f0d46c0b
T2LIPthedeveloper <77957349+T2LIPthedeveloper@users.noreply.github.com>
2023-04-05 21:50:58 +0800

    First commit to profile-page
    Starting layout for profile section

---
+++ app/src/main/java/com/atulparida/spacemate/profile_assets/ProfileActivity.java
@@ -0,0 +1,14 @@
+package com.atulparida.spacemate.profile_assets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
