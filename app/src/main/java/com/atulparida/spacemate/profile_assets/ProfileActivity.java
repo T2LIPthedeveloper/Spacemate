37ff97fd325d9abdd178e787df9614e220ac7754
T2LIPthedeveloper <77957349+T2LIPthedeveloper@users.noreply.github.com>
2023-04-07 01:52:13 +0800

    Fully functional profile screen.
    All we need to do now is hook it up with data from the rest of the screens, through intent passing and UI threading. Still needs to be optimised, but should be okay.
    
    Need to also connect to Firebase whenever possible.

--- app/src/main/java/com/atulparida/spacemate/profile_assets/ProfileActivity.java
+++ app/src/main/java/com/atulparida/spacemate/profile_assets/ProfileActivity.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ViewSwitcher;
 
 import com.atulparida.spacemate.R;
 
@@ -15,57 +20,96 @@ import java.util.List;
 
 public class ProfileActivity extends AppCompatActivity {
     private EditText term, pillar, hostel, gender;
+    private TextView termViewer, pillarViewer, hostelViewer, genderViewer;
+    private ViewSwitcher termVS, pillarVS, hostelVS, genderVS;
     private ImageView termEdit, pillarEdit, hostelEdit, genderEdit;
+
+    private InputMethodManager imm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
 
         initEditTexts();
+        initTextViews();
+        initVS();
         initImageViews();
         initOnClickListeners();
     }
 
-    private void initOnClickListeners() {
-
-        termEdit.setOnClickListener(v -> {
-            if (term.isEnabled()) {
-                setViewingMode(term);
-            } else {
-                setEditingMode(term, this);
-            }
-        });
-
-        pillarEdit.setOnClickListener(v -> {
-            if (pillar.isEnabled()) {
-                setViewingMode(pillar);
-            } else {
-                setEditingMode(pillar, this);
-            }
-        });
-
-        hostelEdit.setOnClickListener(v -> {
-            if (hostel.isEnabled()) {
-                setViewingMode(hostel);
-            } else {
-                setEditingMode(hostel, this);
-            }
-        });
-
-        genderEdit.setOnClickListener(v -> {
-            if (gender.isEnabled()) {
-                setViewingMode(gender);
-            } else {
-                setEditingMode(gender, this);
-            }
-        });
+    private void initVS() {
+        termVS = findViewById(R.id.term_vs);
+        pillarVS = findViewById(R.id.pillar_vs);
+        hostelVS = findViewById(R.id.hostel_vs);
+        genderVS = findViewById(R.id.gender_vs);
     }
 
-    private void initImageViews() {
-        termEdit = findViewById(R.id.termEdit);
-        pillarEdit = findViewById(R.id.pillarEdit);
-        hostelEdit = findViewById(R.id.hostelEdit);
-        genderEdit = findViewById(R.id.genderEdit);
+    private void initTextViews() {
+        termViewer = findViewById(R.id.termValueViewer);
+        pillarViewer = findViewById(R.id.pillarValueViewer);
+        hostelViewer = findViewById(R.id.hostelValueViewer);
+        genderViewer = findViewById(R.id.genderValueViewer);
+    }
+
+    private void initOnClickListener(ImageView itemEdit, TextView textViewer, ViewSwitcher vs, EditText text) {
+        itemEdit.setOnClickListener(v -> {
+            if (!text.isEnabled()) {
+                vs.showNext();
+                text.setEnabled(true);
+
+                if (term.equals(text)) {
+                    text.setRawInputType(InputType.TYPE_CLASS_NUMBER);
+                    text.setHint("1 to 8 for term, 9 for alumni");
+                }
+                else if (pillar.equals(text)) {
+                    text.setRawInputType(InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS);
+                    text.setHint("ASD, CSD, DAI, EPD, or ESD");
+                }
+                else if (hostel.equals(text)) {
+                    text.setRawInputType(InputType.TYPE_TEXT_FLAG_CAP_WORDS);
+                    text.setHint("Yes or No");
+                }
+                else if (gender.equals(text)) {
+                    text.setRawInputType(InputType.TYPE_TEXT_FLAG_CAP_WORDS);
+                    text.setHint("Male, Female, Other");
+                }
+                text.setText("");
+                text.requestFocus();
+                imm.showSoftInput(text, InputMethodManager.SHOW_FORCED);
+            } else {
+                vs.showPrevious();
+                text.setEnabled(false);
+                try {
+                    if (term.equals(text)) {
+                        if (!text.getText().toString().isEmpty() && (Integer.parseInt(text.getText().toString()) > 0 && Integer.parseInt(text.getText().toString()) < 8)) {
+                            textViewer.setText(("Term " + term.getText().toString().trim()).trim());
+                        }
+                        else if (Integer.parseInt(term.getText().toString()) == 9) {
+                            textViewer.setText("Alumni");
+                        }
+                    }
+                    else if (pillar.equals(text)) {
+                        if (text.getText().toString().trim() == "ASD" || text.getText().toString().trim() == "CSD" || text.getText().toString().trim() == "EPD" || text.getText().toString().trim() == "ESD" || text.getText().toString().trim() == "DAI") {
+                            textViewer.setText(text.getText().toString().trim());
+                        }
+                    }
+                    else if (hostel.equals(text)) {
+                        if (text.getText().toString().trim() == "Yes" || text.getText().toString().trim() == "No") {
+                            textViewer.setText(text.getText().toString().trim());
+                        }
+                    }
+                    else if (gender.equals(text)) {
+                        if (text.getText().toString().trim() == "Male" || text.getText().toString().trim() == "Female" || text.getText().toString().trim() == "Other") {
+                            textViewer.setText(text.getText().toString().trim());
+                        }
+                    }
+                }
+                catch (Exception e) {
+                    Toast.makeText(ProfileActivity.this, "Invalid value", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     private void initEditTexts() {
@@ -73,16 +117,27 @@ public class ProfileActivity extends AppCompatActivity {
         pillar = findViewById(R.id.pillarValue);
         hostel = findViewById(R.id.hostelValue);
         gender = findViewById(R.id.genderValue);
+
+        term.setEnabled(false);
+        pillar.setEnabled(false);
+        hostel.setEnabled(false);
+        gender.setEnabled(false);
     }
 
-    public static void setViewingMode(final EditText mEditView) {
-        mEditView.setEnabled(false);
-        mEditView.setText(mEditView.getText().toString().trim());
-        }
+    private void initOnClickListeners() {
+        initOnClickListener(termEdit, termViewer, termVS, term);
+        initOnClickListener(pillarEdit, pillarViewer, pillarVS, pillar);
+        initOnClickListener(hostelEdit, hostelViewer, hostelVS, hostel);
+        initOnClickListener(genderEdit, genderViewer, genderVS, gender);
+    }
 
-    public static void setEditingMode(final EditText mEditView, final Context context) {
-        mEditView.setEnabled(true);
-        mEditView.requestFocus();
-        mEditView.setSelection(mEditView.getText().length());
-        }
+
+
+    private void initImageViews() {
+        termEdit = findViewById(R.id.termEdit);
+        pillarEdit = findViewById(R.id.pillarEdit);
+        hostelEdit = findViewById(R.id.hostelEdit);
+        genderEdit = findViewById(R.id.genderEdit);
+    }
 }
+
